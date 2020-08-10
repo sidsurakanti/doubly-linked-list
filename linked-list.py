f90bc9f1d14c8ff0db0aad9f97b51b78d795c68b
one <60633559+one-wq@users.noreply.github.com>
2020-08-10 11:23:22 -0400

    added more complex stuff

--- linked-list.py
+++ linked-list.py
@@ -8,6 +8,7 @@ class LinkedList:
         # size of the linked list, default to zero
         self.size = 0
 
+
     # adds a new node to the end of the linked list
     def append(self, obj):
         node = Node(obj)
@@ -19,7 +20,8 @@ class LinkedList:
         current.next.prev = current
         self.size += 1
 
-    # prints the linked list
+
+    # prints the linked list as a regular list
     def display(self):
         items = []
         current = self.head
@@ -28,27 +30,73 @@ class LinkedList:
             current = current.next
         print(items)
 
+
     # returns the length of the linked list
     def length(self):
         return self.size
 
-    # deletes the node at (param:index)
-    def delete(self, index=0):
+
+    # returns the data of the node at (param:index)
+    def get(self, index=0):
         count = 0
         current = self.head
         while count != index:
             current = current.next
             count += 1
+        return current.next.data
+    
 
-        current.next = current.next.next
-        current.next.next.prev = current
+    # deletes the node at (param:index)
+    def delete(self, index=0):
+        count = 0
+        last_node = self.head
+        while True:
+            current_node = last_node.next
+            if count == index:
+                last_node.next = current_node.next
+                current_node.next = last_node.prev
+                return
+
+            count += 1
+            last_node = last_node.next
         self.size -= 1
+    
+
+    # count the number of time (param:data) appears in the linked list
+    def count(self, data):
+        count = 0
+        current = self.head
+        while current.next != None:
+            if current.next.data == data:
+                count += 1
+            current = current.next
+        
+        return count
+    
+
+    # replaces object at (param:index) with (param:data)
+    def change(self, index=0, data=None):
+        current = self.head
+        count = 0
+        while count != index:
+            current = current.next
+            count += 1
+        current.next.data = data
+    
+
+    # deletes the item at (param:index) and returns it
+    def pop(self, index=0):
+        popped_node = self.get(index)
+        self.delete(index)
+        return popped_node
+    
+
+    # allows the use of [] to get the item at (param:index) instead of using (method:self.get())
+    def __getitem__(self, index=0):
+        return self.get(index)
 
 
-ll = LinkedList()
-ll.append(1)
-ll.display()
-print(ll.length())
-ll.append(2)
-ll.display()
-print(ll.length())
+    # allows you to use the len() function to find the length of the linked list
+    def __len__(self):
+        return self.length()
+    
