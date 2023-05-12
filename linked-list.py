df901390c3cc381710c498501079756df105e321
Sid <sidsurakanti1@gmail.com>
2023-05-12 14:10:38 -0400

    pep8

--- linked-list.py
+++ linked-list.py
@@ -8,7 +8,6 @@ class LinkedList:
         # size of the linked list, default to zero
         self.size = 0
 
-
     # adds a new node to the end of the linked list
     def append(self, obj):
         node = Node(obj)
@@ -20,7 +19,6 @@ class LinkedList:
         current.next.prev = current
         self.size += 1
 
-
     # prints the linked list as a regular list
     def display(self):
         items = []
@@ -30,11 +28,9 @@ class LinkedList:
             current = current.next
         print(items)
 
-
     # returns the length of the linked list
     def length(self):
         return self.size
-
 
     # returns the data of the node at (param:index)
     def get(self, index=0):
@@ -46,7 +42,6 @@ class LinkedList:
                 count += 1
             return current.next.data
         else: IndexError
-    
 
     # deletes the node at (param:index)
     def delete(self, index=0):
@@ -62,7 +57,6 @@ class LinkedList:
             count += 1
             last_node = last_node.next
         self.size -= 1
-    
 
     # count the number of time (param:data) appears in the linked list
     def count(self, data):
@@ -74,7 +68,6 @@ class LinkedList:
             current = current.next
         
         return count
-    
 
     # replaces object at (param:index) with (param:data)
     def change(self, index=0, data=None):
@@ -84,9 +77,6 @@ class LinkedList:
             current = current.next
             count += 1
         current.next.data = data
-    
-
-
 
     # deletes the item at (param:index) and returns it
     def pop(self, index=0):
@@ -94,12 +84,9 @@ class LinkedList:
         self.delete(index)
         return popped_node
     
-
-
     # allows the use of [] to get the item at (param:index) instead of using (method:self.get())
     def __getitem__(self, index=0):
         return self.get(index)
-
 
     # allows you to use the len() function to find the length of the linked list
     def __len__(self):
@@ -111,11 +98,11 @@ class LinkedList:
             return True
         else:
             return False
-    
 
     # checks if (param:self) is greater than or equal to (param:value)
     def __ge__(self, value):
         return True if self >= value else False
+    
     # checks if (param:self) is greater than (param:value)
     def __gt__(self, value):
         return True if self > value else False
@@ -123,11 +110,11 @@ class LinkedList:
     # checks if (param:self) is less than or equal to (param:value)
     def __le__(self, value):
         return True if self <= value else False
-
-
+    
     # checks if (param:self) is less than or equal to (param:value)
     def __lt__(self, value):
         return True if self < value else False
+    
 
 ll = LinkedList()
 ll.append(2)
